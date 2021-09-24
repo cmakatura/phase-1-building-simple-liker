695cbab2aef4eefb4e1078256557376eeb5e9bfb
christina makatura <cnmak@comcast.net>
2021-09-24 12:56:14 -0500

    commit tree 7f95b1fb29a5b93b69fe54255625a9ea2196120f

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let callServerAndCatch = () => {
+  mimicServerCall()
+  .catch(error => handleError(error))
+}
 
+let handleError = (errorMessage) => {
+modal.classList.remove('hidden')
+let p = document.createElement('p')
+p.innerText = errorMessage
+modal.appendChild(p)
+setTimeout(() => {
+  modal.classList.add('hidden')}, 3000)
+}
+
+let emptyHeartsArray = [...document.getElementsByClassName("like-glyph")]
+//let emptyHeartsArray = [...emptyHearts]
+console.log(emptyHeartsArray)
+
+for (let index = 0; index < emptyHeartsArray.length; index++){
+  emptyHeartsArray[index].addEventListener('click', callServerAndCatch, false);
+}
 
 
 
